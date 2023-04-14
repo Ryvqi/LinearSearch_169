1718d322ee6096a26cc3fbc10938a82e550a6017
Ryvqi <rifqikhabibi03@gmail.com>
2023-04-14 10:32:41 +0700

    Add project files.

---
+++ LinearSearch_169/LinearSearch_169.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int arr[20];  // Array to be searched
+int n;        //Number of elements in the array
+int i;        //Index of array element
+
+
+int main()
+{
+    
+}
